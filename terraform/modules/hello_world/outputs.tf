89031471a2c1c20b93fbb84a9529d4bbf8ea14d0
Matt Jones <msjones@thoughtworks.com>
2018-11-05 17:41:52 +0000

    add some flyway

--- terraform/modules/hello_world/outputs.tf
+++ terraform/modules/hello_world/outputs.tf
@@ -1,3 +1,7 @@
 output "api_gateway_url" {
   value = "${aws_api_gateway_deployment.hello_world_api_deployment.invoke_url}"
 }
+
+output "db_url" {
+  value = "${aws_db_instance.database.endpoint}"
+}
--- terraform/providers/aws/deployment_environment/outputs.tf
+++ terraform/providers/aws/deployment_environment/outputs.tf
@@ -1,3 +1,6 @@
 output "api_gateway_url" {
   value = "${module.hello-world.api_gateway_url}"
 }
+output "db_url" {
+  value = "${module.hello-world.db_url}"
+}
