c9b2006177f6dfa8c9e674d1edb9d618404ebbca
Matt Jones <msjones@thoughtworks.com>
2018-11-05 19:41:27 +0000

    set to allow all incoming and outgoing traffic

--- terraform/modules/hello_world/main.tf
+++ terraform/modules/hello_world/main.tf
@@ -108,9 +108,16 @@ resource "aws_security_group" "allow_all" {
   description = "Allow all inbound traffic"
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "tcp"
+    to_port = 56635
     cidr_blocks = ["0.0.0.0/0"]
   }
 
