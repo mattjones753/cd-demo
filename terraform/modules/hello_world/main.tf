16b4a91b4bd0423ac1c56e41160e314fcaed7de8
Matt Jones <msjones@thoughtworks.com>
2018-11-06 10:44:45 +0000

    try and fix database access

--- terraform/modules/hello_world/main.tf
+++ terraform/modules/hello_world/main.tf
@@ -139,6 +139,6 @@ resource "aws_db_instance" "database" {
   password            = "rubbishpassword"
   skip_final_snapshot = "true"
   publicly_accessible = "false"
-  db_subnet_group_name = "${aws_db_subnet_group.default}"
+  db_subnet_group_name = "${aws_db_subnet_group.default.name}"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
 }
