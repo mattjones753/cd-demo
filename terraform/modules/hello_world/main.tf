094cc17c6b8a96e1680a67c610b9db945a1b3803
Matt Jones <msjones@thoughtworks.com>
2018-11-06 09:46:31 +0000

    make db not publicly accessible

--- terraform/modules/hello_world/main.tf
+++ terraform/modules/hello_world/main.tf
@@ -138,6 +138,6 @@ resource "aws_db_instance" "database" {
   username            = "dbadmin"
   password            = "rubbishpassword"
   skip_final_snapshot = "true"
-  publicly_accessible = "true"
+  publicly_accessible = "false"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
 }
